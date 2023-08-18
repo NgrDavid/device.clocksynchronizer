83761608ca9687fa4c3f93a544c407f41278b0ee
glopesdev <glopesdev@users.noreply.github.com>
2023-08-18 15:27:17 +0100

    Add generated device interface

---
+++ Interface/Harp.ClockSynchronizer/AsyncDevice.Generated.cs
@@ -0,0 +1,48 @@
+using Bonsai.Harp;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Harp.ClockSynchronizer
+{
+    /// <inheritdoc/>
+    public partial class Device
+    {
+        /// <summary>
+        /// Initializes a new instance of the asynchronous API to configure and interface
+        /// with ClockSynchronizer devices on the specified serial port.
+        /// </summary>
+        /// <param name="portName">
+        /// The name of the serial port used to communicate with the Harp device.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous initialization operation. The value of
+        /// the <see cref="Task{TResult}.Result"/> parameter contains a new instance of
+        /// the <see cref="AsyncDevice"/> class.
+        /// </returns>
+        public static async Task<AsyncDevice> CreateAsync(string portName)
+        {
+            var device = new AsyncDevice(portName);
+            var whoAmI = await device.ReadWhoAmIAsync();
+            if (whoAmI != Device.WhoAmI)
+            {
+                var errorMessage = string.Format(
+                    "The device ID {1} on {0} was unexpected. Check whether a ClockSynchronizer device is connected to the specified serial port.",
+                    portName, whoAmI);
+                throw new HarpException(errorMessage);
+            }
+
+            return device;
+        }
+    }
+
+    /// <summary>
+    /// Represents an asynchronous API to configure and interface with ClockSynchronizer devices.
+    /// </summary>
+    public partial class AsyncDevice : Bonsai.Harp.AsyncDevice
+    {
+        internal AsyncDevice(string portName)
+            : base(portName)
+        {
+        }
+    }
+}
---
+++ Interface/Harp.ClockSynchronizer/Device.Generated.cs
@@ -0,0 +1,63 @@
+using Bonsai;
+using Bonsai.Harp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+
+namespace Harp.ClockSynchronizer
+{
+    /// <summary>
+    /// Generates events and processes commands for the ClockSynchronizer device connected
+    /// at the specified serial port.
+    /// </summary>
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    [Description("Generates events and processes commands for the ClockSynchronizer device.")]
+    public partial class Device : Bonsai.Harp.Device, INamedElement
+    {
+        /// <summary>
+        /// Represents the unique identity class of the <see cref="ClockSynchronizer"/> device.
+        /// This field is constant.
+        /// </summary>
+        public const int WhoAmI = 1152;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Device"/> class.
+        /// </summary>
+        public Device() : base(WhoAmI) { }
+
+        string INamedElement.Name => nameof(ClockSynchronizer);
+
+        /// <summary>
+        /// Gets a read-only mapping from address to register type.
+        /// </summary>
+        public static new IReadOnlyDictionary<int, Type> RegisterMap { get; } = new Dictionary<int, Type>
+            (Bonsai.Harp.Device.RegisterMap.ToDictionary(entry => entry.Key, entry => entry.Value))
+        {
+        };
+    }
+
+    /// <summary>
+    /// Represents an operator that groups the sequence of <see cref="ClockSynchronizer"/>" messages by register type.
+    /// </summary>
+    [Description("Groups the sequence of ClockSynchronizer messages by register type.")]
+    public partial class GroupByRegister : Combinator<HarpMessage, IGroupedObservable<Type, HarpMessage>>
+    {
+        /// <summary>
+        /// Groups an observable sequence of <see cref="ClockSynchronizer"/> messages
+        /// by register type.
+        /// </summary>
+        /// <param name="source">The sequence of Harp device messages.</param>
+        /// <returns>
+        /// A sequence of observable groups, each of which corresponds to a unique
+        /// <see cref="ClockSynchronizer"/> register.
+        /// </returns>
+        public override IObservable<IGroupedObservable<Type, HarpMessage>> Process(IObservable<HarpMessage> source)
+        {
+            return source.GroupBy(message => Device.RegisterMap[message.Address]);
+        }
+    }
+}
